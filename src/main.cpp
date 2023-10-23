91a6f9c69035fab850ea1d8ec2aa14671bf85f25
Gabriella <gabrielladelimadev@gmail.com>
2023-10-23 10:28:29 -0300

    feat: arquivo main

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#define SUCCESS (0)
+#define FAILURE (1)
+
+int main(int argc, char const *argv[]){
+    std::string funcao(argv[1]);
+    std::string vertices(argv[2]);
+    
+
+    return SUCCESS;
+}
